91fdbedb1a2a08edd2b674f4106ab4de377d3638
HudayaW <hudayamuhammadrizqi@gmail.com>
2025-05-06 18:18:55 +0700

    acces modifier plus void input

---
+++ pointer.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class PersegiPanjang
+{
+public: //akses modifier
+    int panjang, lebar;
+
+public:
+    void input()
+    {
+        cout << "Masukkan panjang:";
+        cin >> panjang;
+        cout << "Masukkan lebar:";
+        cin >> lebar ;
+    }
+
+}
